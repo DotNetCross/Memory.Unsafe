61ac46d28cb973c1216e2747250e7487bba88e87
ntr <ntr@ihfood.dk>
2016-01-16 11:29:25 +0100

    Added basic .NET Core project to generate baseline IL, to be edited by hand

---
+++ src/DotNetCross.Memory.Unsafe/Unsafe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace DotNetCross.Memory.Unsafe
+{
+    // NOTE: Code here only guide the generation of IL, that is edited by hand
+    public static class Unsafe
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe T Read<T>(void* p)
+            where T : struct
+        {
+            return default(T);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void Write<T>(void* p, T value)
+            where T : struct
+        { }
+    }
+}
