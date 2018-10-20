5cbf9d43c74b9fbd95a975edaea0c07a384e3323
ntr <ntr@ihfood.dk>
2018-10-20 19:39:30 +0200

    Move ref assembly

---
+++ ref/DotNetCross.Memory.Unsafe/Unsafe.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DotNetCross.Memory
+{
+    // NOTE: Code here only to guide the generation of IL, that is edited by hand
+    public static class Unsafe
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe T Read<T>(void* p) { return default(T); } // Overridden in IL
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void Write<T>(void* p, T value) { } // Overridden in IL
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void Write<T>(void* p, ref T value) { } // Overridden in IL
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe int SizeOf<T>() { return 0; } // Overridden in IL
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T As<T>(object obj) where T : class
+        { return default(T); } // Overridden in IL
+
+        // Instead of AddressOf
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void* AsPointer<T>(ref T value) { return (void*)0; } // Overridden in IL
+
+        // Not currently possible in C#
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //public static unsafe ref TTo AsRef<TFrom, TTo>(ref TFrom value) { return default(TTo); } // Overridden in IL
+
+        // Not sure how this can be done
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //public static unsafe TTo AsValue<TFrom, TTo>(TFrom value) 
+        //    where TFrom : struct
+        //    where TTo : struct
+        //{ return default(TTo); } // Overridden in IL
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void InitBlock(void* dst, byte initValue, uint size) { }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void CopyBlock(void* dst, void* src, uint size) { }
+    }
+
+    public sealed class Pinnable
+    {
+        public byte Pin;
+    }
+}
