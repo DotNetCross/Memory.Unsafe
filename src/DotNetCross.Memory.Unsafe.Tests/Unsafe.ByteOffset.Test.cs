7f1a67e066a7c9931c47fef99db1337bf9a656c9
ntr <ntrasmussen@gmail.com>
2018-12-23 15:20:23 +0100

    Add array test

---
+++ src/DotNetCross.Memory.Unsafe.Tests/Unsafe.ByteOffset.Test.cs
@@ -0,0 +1,42 @@
+using System;
+using Xunit;
+
+namespace DotNetCross.Memory.Tests
+{
+    public class Unsafe_ByteOffset_Test
+    {
+        [Fact]
+        public static unsafe void ByteOffset_BoxedInt()
+        {
+            var obj = (object)42;
+            ref var valueRef = ref Unsafe.Unbox<int>(obj);
+            
+            var valueByteOffset = Unsafe.ByteOffset(obj, ref valueRef);
+
+            ref var byteOffsetValueRef = ref Unsafe.AsRefAtByteOffset<int>(obj, valueByteOffset);
+            byteOffsetValueRef = 17;
+
+            Assert.True(Unsafe.AreSame(ref valueRef, ref byteOffsetValueRef));
+            Assert.Equal(17, (int)obj);
+            // Currently, we can assume that the offset inside the object is equal
+            // to the pointer size. This is highly runtime dependent though.
+            Assert.Equal((long)Unsafe.SizeOf<IntPtr>(), (long)valueByteOffset);
+            Assert.True((long)valueByteOffset >= (long)Unsafe.SizeOf<IntPtr>());
+        }
+
+        [Fact]
+        public static unsafe void ByteOffset_Array()
+        {
+            var array = new int[] { 0,1,2,3,4,5,6 };
+            ref var valueRef = ref array[2];
+
+            var valueByteOffset = Unsafe.ByteOffset(array, ref valueRef);
+
+            ref var byteOffsetValueRef = ref Unsafe.AsRefAtByteOffset<int>(array, valueByteOffset);
+            byteOffsetValueRef = 17;
+
+            Assert.True(Unsafe.AreSame(ref valueRef, ref byteOffsetValueRef));
+            Assert.Equal(17, array[2]);
+        }
+    }
+}
