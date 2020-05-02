c1b81fedc197a2f9ed2eb26da210a01d4d71643f
ntr <ntr@ihfood.dk>
2020-05-02 18:05:05 +0200

    cleanup

--- src/DotNetCross.Memory.Unsafe.Benchmark/BasicReadWriteBench.cs
+++ src/DotNetCross.Memory.Unsafe.Benchmark/BasicReadWriteBench.cs
@@ -1,26 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using BenchmarkDotNet;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Engines;
-using BenchmarkDotNet.Jobs;
-using DotNetCross.Memory;
-
+﻿using BenchmarkDotNet.Attributes;
 
 namespace DotNetCross.Memory.Benchmark
 {
     public struct Bgr { public byte B; public byte G; public byte R; }
 
-    //[SimpleJob(RuntimeMoniker.Net461, warmupCount: 2, targetCount: 11, baseline: true)]
-    //[SimpleJob(RuntimeMoniker.NetCoreApp21, warmupCount: 2, targetCount: 11)]
-    //[SimpleJob(RuntimeMoniker.NetCoreApp31, warmupCount: 2, targetCount: 11)]
-    //[LegacyJitX86Job, LegacyJitX64Job, RyuJitX86Job, RyuJitX64Job]
     public class BasicReadWriteBench<T>
         where T : struct
     {
-
         // NOTE: This includes cost of stack alloc
         [Benchmark]
         public void ReadFromStack()
--- src/DotNetCross.Memory.Unsafe.Benchmark/Program.cs
+++ src/DotNetCross.Memory.Unsafe.Benchmark/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using BenchmarkDotNet;
-using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
